4f8319416f28b6522c91a09633ccd313cb9732d7
hamfirst <nick.weihs@gmail.com>
2018-02-01 03:24:38 -0800

    Fix compile issues on linux

--- External/usrsctplib/netinet/ifaddrs.h
+++ External/usrsctplib/netinet/ifaddrs.h
@@ -32,7 +32,7 @@ struct ifaddrs {
 	unsigned int	 ifa_flags;
 	struct sockaddr	*ifa_addr;
 	struct sockaddr	*ifa_netmask;
-	struct sockaddr	*ifa_dstaddr;
+	//struct sockaddr * ifa_dstaddr;
 	void		*ifa_data;
 };
 
